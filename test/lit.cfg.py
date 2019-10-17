df71b14d74f64eb98c87d4f9cce5792a83b0c620
Andrzej Warzynski <andrzej.warzynski@gmail.com>
2019-10-17 22:19:02 +0100

    [NFC] Refine CMake, remove redundant suffic in LIT config

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -23,7 +23,7 @@ config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files. This is overriden
 # by individual lit.local.cfg files in the test subdirectories.
-config.suffixes = ['.ll', '.c']
+config.suffixes = ['.ll']
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
