52c3034663d88640c2f7e833fa3b2625b7d07ce4
FinleyTang <71133371+FinleyTang@users.noreply.github.com>
2023-11-19 21:35:24 +0800

    Revert "delete legacy PM example"
    This reverts commit 5fbde6c42242c8d043cfa356293d3ee62a99bad4.

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -51,6 +51,19 @@ struct HelloWorld : PassInfoMixin<HelloWorld> {
   static bool isRequired() { return true; }
 };
 
+// Legacy PM implementation
+struct LegacyHelloWorld : public FunctionPass {
+  static char ID;
+  LegacyHelloWorld() : FunctionPass(ID) {}
+  // Main entry point - the name conveys what unit of IR this is to be run on.
+  bool runOnFunction(Function &F) override {
+    visitor(F);
+    // Doesn't modify the input unit of IR, hence 'false'
+    return false;
+  }
+};
+} // namespace
+
 //-----------------------------------------------------------------------------
 // New PM Registration
 //-----------------------------------------------------------------------------
@@ -76,3 +89,19 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
   return getHelloWorldPluginInfo();
 }
+
+//-----------------------------------------------------------------------------
+// Legacy PM Registration
+//-----------------------------------------------------------------------------
+// The address of this variable is used to uniquely identify the pass. The
+// actual value doesn't matter.
+char LegacyHelloWorld::ID = 0;
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// recognize LegacyHelloWorld when added to the pass pipeline on the command
+// line, i.e.  via '--legacy-hello-world'
+static RegisterPass<LegacyHelloWorld>
+    X("legacy-hello-world", "Hello World Pass",
+      true, // This pass doesn't modify the CFG => true
+      false // This pass is not a pure analysis pass => false
+    );
